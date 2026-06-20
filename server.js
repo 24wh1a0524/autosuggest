606b92fa2a8f534697f39f9d993476a19e6f6eee
P.Shiva sai <shivasaiperuka123@gmail.com>
2026-06-20 11:03:26 +0530

    Task completed

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const app = express();
 const port= process.env.PORT || 6700;
 
 // To tell app, where static files -html/css/js- are stored, so that it can serve them to the client
-app.use(express.static("frontend"));
+app.use(express.static("frentend"));
 
 app.listen(port, function() {
   console.log("App running on http://localhost:" + port);
 });
-
